75205fdecb3d74ae6a1cc6c738d66d84a9b0e682
Gleb <lyubin24@gmail.com>
2025-06-08 21:05:20 +0800

    initial code for paging

---
+++ kernel/include/kernel/frames.h
@@ -0,0 +1,7 @@
+#ifndef FRAMES_H
+#define FRAMES_H
+
+#include <stdint.h>
+
+
+#endif
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <kernel/tty.h>
 #include <test/test.h>
+#include <stdint.h>
+
+extern uint32_t endkernel;
+
 
 void kernel_main(void) {
 	terminal_initialize();
-	// printf("1\n");
-	// printf("2\n");
-	// printf("3\n");
-	// printf("4\n");
-	// printf("5\n");
-	// printf("6\n");
 
 	int val = 3223;
 	printf("%d\n", val);
 }
+
+
