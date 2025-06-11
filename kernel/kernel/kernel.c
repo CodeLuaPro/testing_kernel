5f081cd4bdcc315e8905c6da7355685dc4d318cd
Gleb <lyubin24@gmail.com>
2025-06-11 19:44:48 +0800

    using grub instead of bios now

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,12 +2,45 @@
 #include <kernel/tty.h>
 #include <test/test.h>
 #include <stdint.h>
+#include <kernel/multiboot.h>
 
 extern uint32_t endkernel;
 
 
-void kernel_main(void) {
+void kernel_main(multiboot_info_t* mbd, uint32_t magic) {
+
 	terminal_initialize();
+
+	/* Make sure the magic number matches for memory mapping*/
+    if(magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        printf("invalid magic number!\n");
+    }
+
+    /* Check bit 6 to see if we have a valid memory map */
+    if(!(mbd->flags >> 6 & 0x1)) {
+        printf("invalid memory map given by GRUB bootloader\n");
+    }
+
+    /* Loop through the memory map and display the values */
+    int i;
+    for(i = 0; i < mbd->mmap_length; 
+        i += sizeof(multiboot_memory_map_t)) 
+    {
+        multiboot_memory_map_t* mmmt = 
+            (multiboot_memory_map_t*) (mbd->mmap_addr + i);
+
+        printf("Size: %x | Type: %d\n",
+            mmmt->size, mmmt->type);
+
+        if(mmmt->type == MULTIBOOT_MEMORY_AVAILABLE) {
+            /* 
+             * Do something with this memory block!
+             * BE WARNED that some of memory shown as availiable is actually 
+             * actively being used by the kernel! You'll need to take that
+             * into account before writing to memory!
+             */
+        }
+    }
 
 	int val = 3223;
 	printf("%d\n", val);
